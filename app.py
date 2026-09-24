02ec807cebc0737fef939a9581a9a1a024b9e386
Giancsa <gian.69889868@gmail.com>
2026-09-23 21:20:14 -0300

    2026.09.23

--- app.py
+++ app.py
@@ -1,10 +1,57 @@
-from flask import Flask, render_template
+from flask import Flask, abort, render_template
+from flask_bootstrap import Bootstrap5
+import sqlite3
 
 app = Flask(__name__)
 
+from flask import Flask, render_template
+import sqlite3
+
+app = Flask(__name__)
+
+
 @app.route("/")
 def index():
-    return render_template('index.html')
+
+    with sqlite3.connect('database.db') as conn:
+        conn.row_factory = sqlite3.Row
+        contents = conn.execute("""
+            SELECT id, name, photo 
+            FROM thing
+                WHERE status = 'on'
+                ORDER BY created_at
+        """).fetchall()
+
+    total = len(contents)
+
+    return render_template(
+        'index.html',
+        contents=contents,
+        total=total
+    )
+
+@app.route('/view/<int:thing_id>')
+def view(thing_id):
+
+    with sqlite3.connect('database.db') as conn:
+        conn.row_factory = sqlite3.Row
+        content = conn.execute("""
+            SELECT *
+            FROM thing
+                WHERE status = 'on'
+                AND id = ?
+                ORDER BY created_at
+        """, (thing_id,)).fetchone()
+
+    if content is None:
+        abort(404)
+
+    return render_template(
+        "view.html",
+        content=content
+    )
+
+
 
 @app.route("/new")
 def new_thing():
--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
+# setupdb.py
+# Faz o reset e o setup inicial do banco de dados SELite quando necessário.
+# PERIGO! Use isso somente em "devtime".
+
 import sqlite3
 
+# Abre (ou cria) o banco de dados
 connection = sqlite3.connect("database.db")
 
+# Lê o arquivo SQL modelador do banco de dados
 with open("database.sql", "r", encoding="utf-8") as file:
     sql = file.read()
 
-connection.executescript(sql)    
+# Executa todo o script SQL
+connection.executescript(sql)
 
+# Fecha a conexão
 connection.close()
 
-print("Banco de dados preparado com sucesso")
+# Feedback
+print("Banco de dados preparado com sucesso!")
